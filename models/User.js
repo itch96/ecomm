36c2a3f45d3225a59f1c9c8c922d1c2a31f17ad3
Rahul Gupta <useritch96@gmail.com>
2017-10-02 08:04:14 +0530

    Added the user model

---
+++ models/User.js
@@ -0,0 +1,54 @@
+'use strict';
+const mongoose = require('mongoose'),
+  bcrypt = require('bcryptjs'),
+  crypto = require('crypto');
+
+let Schema = mongoose.Schema;
+
+// The user schema
+let UserSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String },
+  profile: {
+    name: { type: String },
+    picture: { type: String }
+  },
+  address: { type: String },
+  history: [{
+    date: { type: Date, default: Date.now },
+    paid: { type: Number },
+    item: { type: Schema.Types.ObjectId, }
+  }]
+});
+
+// Hash the password before saving
+UserSchema.pre('save', function(next) {
+  var user = this;
+  // if(!user.isModified('password')) return next();
+  
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) {console.log(err); return next(err);}
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) {console.log(err); return next(err);}
+        user.password = hash;
+        next();
+    });
+  });
+});
+
+// Compare the password from the database
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.password, function(err, res) {
+    console.log(res);
+    return res;
+  });
+};
+
+UserSchema.methods.gravatar = function(size) {
+  if(!this.size) size = 200;
+  if(!this.email) return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+}
+
+module.exports = mongoose.model('User', UserSchema);
