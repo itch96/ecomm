c3361dc45ea278e3527929b8506898170e00435a
Rahul Gupta <useritch96@gmail.com>
2017-10-02 08:06:59 +0530

    moved routes from server.js to a seperate folder

---
+++ routes/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+let router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.render('main/home');
+});
+
+router.get('/about', (req, res) => {
+  res.render('main/about');
+});
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,78 @@
+const express = require('express');
+let router = express.Router();
+const passport = require('passport');
+
+let User = require('../models/User');
+let passportConf = require('../config/passport');
+
+router.get('/login', (req, res) => {
+  if(req.user) return res.redirect('/');
+  res.render('accounts/login', { message: req.flash('loginMessage') });
+});
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/profile', (req, res, next) => {
+  User.findOne({ _id: req.user._id }, (err, user) => {
+    if(err) return next(err);
+    res.render('accounts/profile', { user: user });
+  });
+});
+
+router.get('/signup', (req, res) => {
+  if(req.user) return res.redirect('/');
+  res.render('accounts/signup', { errors: req.flash('errors') });
+});
+
+router.post('/signup', (req, res, next) => {
+  let user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+  user.profile.picture = user.gravatar();
+
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
+    if(existingUser) {
+      req.flash('errors', `Account with ${req.body.email} already exists`);
+      return res.redirect('/signup');
+    } else {
+      user.save((err) => {
+        if(err) { return next(err); }
+        req.logIn(user, (err) => {
+          if(err) return next(err);
+          res.redirect('/profile');
+        });
+      });
+    }
+  });
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/edit-profile', (req, res, next) => {
+  res.render('accounts/edit-profile', {message: req.flash('success')});
+});
+
+router.post('/edit-profile', (req, res, next) => {
+  User.findOne({ _id: req.user._id }, (err, user) => {
+    if(err) return next(err);
+    if(req.body.name) user.profile.name = req.body.name;
+    if(req.body.address) user.address = req.body.address;
+
+    user.save((err) => {
+      if(err) return next(err);
+      req.flash('success', 'Successfully edited profile.');
+      return res.redirect('/edit-profile');
+    });
+  });
+});
+
+module.exports = router;
