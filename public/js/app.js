374a419365f79a11a2bae55cb6fc2a45b17d949e
Rahul Gupta <useritch96@gmail.com>
2017-10-07 23:14:38 +0530

    Added the card ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,34 @@
 var main = function() {
   $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $('.modal').modal();
+  
+  $(document).on('click', '#plus', function(event) {
+    event.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+    
+    priceValue += parseFloat($('#priceHidden').val());
+    quantity += 1;
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  });
+
+  $(document).on('click', '#minus', function(event) {
+    event.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+    
+    if(quantity > 1) {
+      priceValue -= parseFloat($('#priceHidden').val());
+      quantity -= 1;
+
+      $('#quantity').val(quantity);
+      $('#priceValue').val(priceValue.toFixed(2));
+      $('#total').html(quantity);
+    }
+  });
 }
 $(document).ready(main);
