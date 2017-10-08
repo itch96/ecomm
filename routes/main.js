6806c37d177c1253e0383709c0ba9552578addaa
Rahul Gupta <useritch96@gmail.com>
2017-10-08 14:57:49 +0530

    Added the main and the user routes

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,186 @@
-'use strict';
-
 const express = require('express');
 let router = express.Router();
+let secret = require('../config/secret');
+const async = require('async');
+const stripe = require('stripe')(secret.stripeSecretKey);
 
+let Product = require('../models/Product');
+let Cart = require('../models/Cart');
+let User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.render('main/home');
+function paginate(req, res, callback, currentPage) {
+  let perPage = 12;
+  let page = req.params.page;
+
+  Product
+    .find()
+    .skip(perPage * page)
+    .limit(perPage)
+    .populate('category')
+    .exec((err, products) => {
+      if(err) return callback(err);
+      Product.count().exec((err, count) => {
+        if(err) return callback(err);
+        res.render('main/product-main', {
+          products: products,
+          pages: count / perPage,
+          currentPage: currentPage
+        });
+      });
+    });
+}
+
+// create a map b/w Product Schema and elasticsearch replica set.
+Product.createMapping(function(err, mapping) {
+  if(err) {
+    console.log("Error creating mapping.");
+    console.log(err);
+  } else {
+    console.log("Mapping created.");
+    console.log(mapping);
+  }
+});
+
+let stream = Product.synchronize(); // replicate all the data and put it in elasticsearch
+let count = 0;
+
+stream.on('data', function() {
+  count ++;
+});
+stream.on('close', function() {
+  console.log('Indexed ' + count + ' document.');
+});
+stream.on('error', function(err) {
+  console.log(err);
+});
+
+router.post('/products/:product_id', (req, res, callback) => {
+  Cart.findOne({ owner: req.user._id }, (err, cart) => {
+    cart.items.push({
+      item: req.body.product_id,
+      price: parseFloat(req.body.priceValue),
+      quantity: parseFloat(req.body.quantity  )
+    });
+
+    cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
+    cart.save((err) => {
+      if(err) return callback(err);
+      return res.redirect('/cart');
+    });
+  });
+});
+
+router.post('/remove', (req, res, callback) => {
+  Cart.findOne({ owner: req.user._id }, (err, foundCart) => {
+    foundCart.items.pull(String(req.body.item));
+
+    foundCart.total = (foundCart.total - parseFloat(req.body.price)).toFixed(2);
+    foundCart.save((err, found) => {
+      if(err) return callback(err);
+      req.flash('remove', 'Successfully removed item from cart');
+      res.redirect('/cart');
+    });
+  });
+});
+
+router.get('/cart', (req, res, callback) => {
+  Cart.findOne({ owner: req.user._id })
+    .populate('items.item')
+    .exec(function(err, foundCart) {
+      if(err) return callback(err);
+      res.render('main/cart', {
+        foundCart: foundCart,
+        message: req.flash('remove')
+      });
+    });
+});
+
+router.post('/search', (req, res, callback) => {
+  res.redirect('/search?q=' + req.body.query);
+});
+
+router.get('/search', (req, res, callback) => {
+  if(req.query.q) {
+    Product.search({
+      query_string: { query: req.query.q }
+    }, (err, results) => {
+      if(err) return callback(err);
+      let data = results.hits.hits.map((hit) => {return hit;});
+      res.render('main/search-result', {
+        query: req.query.q,
+        data: data
+      });
+    });
+  }
+});
+
+router.get('/', (req, res, callback) => {
+  if(req.user) {
+    paginate(req, res, callback, 1);
+  } else {
+    res.render('main/home');
+  }
+});
+
+router.get('/page/:page', (req, res, callback) => {
+  paginate(req, res, callback, req.params.page);
 });
 
 router.get('/about', (req, res) => {
   res.render('main/about');
+});
+
+router.get('/categories/:id', (req, res, callback) => {
+  Product.find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products) {
+      if(err) return callback(err);
+      res.render('main/category', { products: products });
+    });
+});
+
+router.get('/products/:id', (req, res, callback) => {
+  Product.findOne({ _id: req.params.id })
+    .populate('category')
+    .exec((err, product) => {
+    if(err) return callback(err);
+    res.render('main/product', {product: product});
+  });
+});
+
+router.post('/payment', (req, res, callback) => {
+  // just empty the cart and add the items in the user's history
+  async.waterfall([
+    function(callback) {
+      Cart.findOne({ owner: req.user._id }, function(err, cart) {
+        callback(err, cart);
+      });
+    },
+    function(cart, callback) {
+      User.findOne({ _id: req.user._id }, function(err, user) {
+        if(user) {
+          for(var i = 0; i < cart.items.length; i ++) {
+            user.history.push({
+              item: cart.items[i].item,
+              paid: cart.items[i].price
+            });
+          }
+
+          user.save(function(err, user) {
+            if(err) return callback(err);
+            callback(err, user);
+          });
+        }
+      });
+    },
+    function(user) {
+      Cart.update({ owner: user._id }, { $set: { items: [], total: 0 }}, function(err, updated) {
+        if(updated) {
+          res.redirect('/profile');
+        }
+      });
+    }
+  ]);
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 let router = express.Router();
 const passport = require('passport');
+const async = require('async');
 
 let User = require('../models/User');
+let Cart = require('../models/Cart');
 let passportConf = require('../config/passport');
 
 router.get('/login', (req, res) => {
@@ -16,11 +18,13 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true
 }));
 
-router.get('/profile', (req, res, next) => {
-  User.findOne({ _id: req.user._id }, (err, user) => {
-    if(err) return next(err);
-    res.render('accounts/profile', { user: user });
-  });
+router.get('/profile', passportConf.isAuthenticated, (req, res, next) => {
+  User.findOne({ _id: req.user._id })
+    .populate('history.item')
+    .exec(function(err, foundUser) {
+      if(err) return next(err);
+      res.render('accounts/profile', { user: foundUser });
+    });
 });
 
 router.get('/signup', (req, res) => {
@@ -29,30 +33,43 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', (req, res, next) => {
-  let user = new User();
-
-  user.profile.name = req.body.name;
-  user.password = req.body.password;
-  user.email = req.body.email;
-  user.profile.picture = user.gravatar();
-
-  User.findOne({ email: req.body.email }, (err, existingUser) => {
-    if(existingUser) {
-      req.flash('errors', `Account with ${req.body.email} already exists`);
-      return res.redirect('/signup');
-    } else {
-      user.save((err) => {
-        if(err) { return next(err); }
+  async.waterfall([
+    function(callback) {
+      let user = new User();
+      user.profile.name = req.body.name;
+      user.password = req.body.password;
+      user.email = req.body.email;
+      user.profile.picture = user.gravatar();
+    
+      User.findOne({ email: req.body.email }, (err, existingUser) => {
+        if(existingUser) {
+          req.flash('errors', `Account with ${req.body.email} already exists`);
+          return res.redirect('/signup');
+        } else {
+          user.save((err) => {
+            if(err) { return next(err); }
+            callback(null, user);
+          });
+        }
+      });
+    },
+    function(user) {
+      let cart = new Cart();
+      cart.owner = user._id;
+      cart.save(function(err) {
+        if(err) return next(err);
         req.logIn(user, (err) => {
-          if(err) return next(err);
+          if(err) { return next(err); }
+          console.log(req.user);
           res.redirect('/profile');
         });
       });
     }
-  });
+  ]);
 });
 
 router.get('/logout', (req, res, next) => {
+  console.log('Logout fired');
   req.logout();
   res.redirect('/');
 });
@@ -74,5 +91,13 @@ router.post('/edit-profile', (req, res, next) => {
     });
   });
 });
+
+// by default facebook will not provide email. So add scope: 'email'
+router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/profile',
+  failureFlash: '/login'
+}));
 
 module.exports = router;
